9ecd5ce0f8fcdd707839cdd3ea90c593854c3c66
Prashanth Babu T <prashanthbabu07@gmail.com>
2020-10-26 16:34:59 +0530

    Update learn.hs

--- learn.hs
+++ learn.hs
@@ -400,3 +400,38 @@ thunk = if (1 + 1) `mod` 2 == 0 then (1 + 1) else (1 + 2)
 --  applicatives
 --  monads
 
+data Trivial
+  = Trivial'
+
+instance Eq Trivial where
+  Trivial' == Trivial' = True
+
+data DayOfWeek = Sun | Mon | Tue | Wed | Thu | Fri | Sat
+
+data Date = Date DayOfWeek Int
+
+instance Eq DayOfWeek where
+  (==) Sun Sun = True
+  (==) Mon Mon = True
+  (==) Tue Tue = True
+  (==) Wed Wed = True
+  (==) Thu Thu = True
+  (==) Fri Fri = True
+  (==) Sat Sat = True
+  (==) _ _ = False
+
+instance Show DayOfWeek where
+  show Sun = "Sun"
+  show Mon = "Mon"
+  show Tue = "Tue"
+  show Wed = "Wed"
+  show Thu = "Thu"
+  show Fri = "Fri"
+  show Sat = "Sat"
+
+instance Eq Date where
+  (==) (Date week_day day_of_month) (Date week_day' day_of_month') =
+    week_day == week_day' && day_of_month == day_of_month'
+
+instance Show Date where
+  show (Date week_day day_of_month) = (show week_day) ++ " " ++ (show day_of_month)
